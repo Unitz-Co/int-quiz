d37910b10b5ce4923714b8c4f30ae29524f776e8
DESKTOP-LJ63MPE\anhvi <anhvipkaka9999@gmail.com>
2022-03-03 17:28:06 +0700

    start_up

---
+++ front-end/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import DataJson from "./data/data.json";
+import "./data/data.json";
+function App() {
+  console.log("====================================");
+  console.log(DataJson);
+  console.log("====================================");
+  return (
+    <React.Fragment>
+      <p>Table and me</p>
+    </React.Fragment>
+  );
+}
+
+export default App;
