23ea08ab772e2e03ea1958f65c220b3eddae683e
Nhi Nguyen <nguyenthiyennhi@C02F62W3Q05D.local>
2022-09-17 21:23:28 +0700

    upload final app

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginForm from './src/components/LoginForm';
 
-export default function App() {
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+            {/* <Stack.Screen name="Login" component={LoginForm} options={{ headerShown: false }}/> */}
+            <Stack.Screen name="HomeScreen" component={ HomeScreen } />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/mock-api/status.jsx
@@ -0,0 +1,17 @@
+export const ObjectStatus = [
+    {
+        id: 1,
+        name: 'All',
+        value: 'all',
+    },
+    {
+        id: 2,
+        name: 'Online',
+        value: 'online',
+    },
+    {
+        id: 3,
+        name: 'Offline',
+        value: 'offline',
+    }
+]
