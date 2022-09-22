2b1ff0e6bb9c9557b2ef77ca96378dfdf45553a4
nguyenxuanvinh309 <nguyenxuanvinh309@gmail.com>
2022-09-22 09:36:01 +0700

    feat(unitz-test): int-quiz-nguyenxuanvinh improve search

--- src/utils/commonFunction.js
+++ src/utils/commonFunction.js
@@ -1,16 +1,22 @@
-import { filter, lowerCase } from 'lodash';
+import { filter, lowerCase, get, map, some } from 'lodash';
 
 export function filterList(data = [], params) {
   const { keyWord, status, filterAll } = params.keys;
   let newData = data;
 
+  const categoriesList = item => {
+    const newCategories = map(get(item, 'categoriesCollection.items', []), ele => lowerCase(ele.displayName)) || item;
+
+    return newCategories;
+  }
+  
   if (filterAll) {
     newData = filter(data, item => (
-      lowerCase(item.displayName).includes(keyWord)
+      lowerCase(item.displayName).includes(keyWord) || some(categoriesList(item), ele => ele.includes(keyWord))
     ));
   } else {
     newData = filter(data, item => (
-      item.status === status && lowerCase(item.displayName).includes(keyWord)
+      item.status === status && (lowerCase(item.displayName).includes(keyWord) || some(categoriesList(item), ele => ele.includes(keyWord)))
     ));
   }
 
