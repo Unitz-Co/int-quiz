1dadee4872cfdc979ca23c5ae7326105fc6e536f
phkai16 <phkai16@gmail.com>
2022-10-22 13:45:52 +0700

    first commit solution

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CartList from "./components/cardList/CardList.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <CartList />
+    </div>
+  );
+}
+
+export default App;
